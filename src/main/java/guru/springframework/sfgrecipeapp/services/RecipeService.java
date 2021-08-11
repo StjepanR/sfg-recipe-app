2e8849eb9ae6b5e27f68a0eb09b4c2e95d8e601c
Stjepan Ruklić <sr51641@fer.hr>
2021-08-11 14:06:36 +0200

    Added Recipe Service and changed Index Page.

---
+++ src/main/java/guru/springframework/sfgrecipeapp/services/RecipeService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgrecipeapp.services;
+
+import guru.springframework.sfgrecipeapp.domain.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+    Set<Recipe> getRecipes();
+}
